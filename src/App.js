7694f31ca53f4cc1daf2651b31761b2d0224eb20
warisanwar <warismir07@gmail.com>
2020-10-02 12:33:05 +0530

    added more features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './components/FlashcardList';
+import './App.css';
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(FLASHCARD_DATA)
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setFlashcards(res.data.results.map((item, index) => {
+          const answer = item.correct_answer;
+          const options = [...item.incorrect_answers, answer]
+          return {
+            id: `$${index}-${Date.now()}`,
+            question: item.question,
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+      })
+  }, [])
   return (
     <FlashcardList flashcards={flashcards} />
   );
@@ -18,7 +35,7 @@ const FLASHCARD_DATA = [
       '4',
       '5'
     ],
-    answer: '4'
+    answer: '3'
   },
   {
     id: 2,
@@ -29,7 +46,7 @@ const FLASHCARD_DATA = [
       '4',
       '5'
     ],
-    answer: '5'
+    answer: 'answer'
   }
 ]
 
