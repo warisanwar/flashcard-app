76e0c8669fd9f8ee73c67f18e8f14f6a2c4af9ca
warisanwar <warismir07@gmail.com>
2020-10-08 21:30:36 +0530

    added the filter

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
         </div>
         <div className="form-group">
           <label htmlFor="amount">Number of Questions</label>
-          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+          <input type="number" id="amount" min="1" step="2" defaultValue={10} ref={amountEl} />
         </div>
         <div className="form-group">
           <button className="btn">Generate</button>
